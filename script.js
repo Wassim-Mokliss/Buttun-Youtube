150dbdc23d28e9846629cf570765ff247951b541
Wassim <wasshimu.dev@gmail.com>
2024-11-08 19:58:12 +0100

    Deux elements stocké JS

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log('connecté'); 
+// Je selectionne et je stock l'icone
+const icon = document.getElementsByClassName('far fa-meh-blank');
+// Je selectionne et je stock le boutton
+const btnFollow = document.getElementsByClassName('btn');
+
+console.log(icon, btnFollow);
